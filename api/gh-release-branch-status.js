9c69ffae32890d3e5384833cc30093da76d8a0d3
Ralph Meier <ralphmeier79@gmail.com>
2020-09-29 16:32:06 +0200

    fix: get rid of octokit

--- api/gh-release-branch-status.js
+++ api/gh-release-branch-status.js
@@ -4,8 +4,11 @@ const _ = require('lodash')
 const assert = require('assert')
 const conventionalCommits = require('semantic-release-conventional-commits')
 const {isValidSha, isValidRepository} = require('../lib/validation')
-const OctokitHelper = require('../lib/octokit_helper').OctokitHelper
 const isReleaseBranch = require('../lib/is_release_branch')
+const getPullBySha = require('../lib/git/get_pull_by_sha')
+const getPull = require('../lib/git/get_pull')
+const getCommitByPull = require('../lib/git/get_commit_by_pull')
+const createCommitStatus = require('../lib/git/create_commit_status')
 
 assert(process.env.GITHUB_TOKEN, 'missing environment variable GITHUB_TOKEN e.g 11b22b33n4')
 const token = process.env.GITHUB_TOKEN
@@ -20,33 +23,33 @@ module.exports = async (req, res) => {
   try {
     await run(req, res)
   } catch (error) {
+    console.log('error', error)
     return res.status(400).json({ error: error.message })
   }
 }
 
 async function run (req, res) {
   let state
-  const o = new OctokitHelper(token)
   const {repository, sha} = req.body
 
   if (!isValidRepository(repository, res)) return
   if (!isValidSha(sha, res)) return
 
-  const pullRequests = await o.searchPullRequest({repository, sha})
-  const pullRequestNumber = _.get(pullRequests, 'data.items[0].number', false)
+  const pullRequests = await getPullBySha({repository, token, sha})
+  const pullRequestNumber = _.get(pullRequests, '[0].number', false)
 
   if (!pullRequestNumber) {
     return res.status(401).json({message: `no pull request found with commit ${sha} `})
   }
 
-  const pr = await o.getPullRequest({repository, number: pullRequestNumber})
+  const pr = await getPull({repository, token, pull_number: pullRequestNumber})
 
-  const baseBranchName = _.get(pr, 'data.base.ref', false)
-  const pullRequestUrl = _.get(pr, 'data.html_url', false)
+  const baseBranchName = _.get(pr, 'base.ref', false)
+  const pullRequestUrl = _.get(pr, 'html_url', false)
 
   if (isReleaseBranch(baseBranchName)) {
-    const prComments = await o.getPullRequestCommits({repository, number: pullRequestNumber})
-    const commits = _.map(prComments.data, (c) => c.commit)
+    const prComments = await getCommitByPull({repository, token, pull_number: pullRequestNumber})
+    const commits = _.map(prComments, (c) => c.commit)
 
     const type = await conventionalCommits(commitConfig, {commits})
 
@@ -59,10 +62,6 @@ async function run (req, res) {
     state = 'no-release-branch'
   }
 
-  try {
-    await o.createStatus({repository, sha, state})
-  } catch (e) {
-    return res.status(400).json({message: `failed to update github status '${state}' on pull request ${pullRequestUrl}`})
-  }
-  return res.status(200).json({message: `github check updated with status '${state}' on pull request ${pullRequestUrl} .`})
+  await createCommitStatus({repository, token, sha, state})
+  res.status(200).json({message: `github check updated with status '${state}' on pull request ${pullRequestUrl} .`})
 }
