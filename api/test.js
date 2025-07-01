22156494555072094a37b563f13ca80dfacf65ba
Ralph Meier <ralphmeier79@gmail.com>
2025-07-01 17:01:48 +0200

    fix: deploy version 2

--- api/test.js
+++ api/test.js
@@ -2,5 +2,5 @@ const moment = require('moment')
 
 module.exports = (req, res) => {
   const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
-  res.end(`currentTime: ${currentTime}, version: 1`)
+  res.end(`currentTime: ${currentTime}, version: 2`)
 }
