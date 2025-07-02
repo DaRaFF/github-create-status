5663acde7714054142fe7e6dcc127e43839c42b3
Ralph Meier <ralphmeier79@gmail.com>
2025-07-02 14:18:04 +0200

    fix: add dfv repo

--- api/test.js
+++ api/test.js
@@ -2,5 +2,7 @@ const moment = require('moment')
 
 module.exports = (req, res) => {
   const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
-  res.end(`currentTime: ${currentTime}, version: 2`)
+  res.end(
+    `currentTime: ${currentTime}, version: 3, log: add dfv repository to allowed_repositories.js`
+  )
 }
