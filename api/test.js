5a0d46a43003de32a45466e6f4ff42705fe77ed9
Ralph Meier <ralphmeier79@gmail.com>
2026-02-19 14:53:09 +0100

    fix: added nrk

--- api/test.js
+++ api/test.js
@@ -2,5 +2,5 @@ const moment = require('moment')
 
 module.exports = (req, res) => {
   const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
-  res.end(`currentTime: ${currentTime}, version: 4, log: improved logging`)
+  res.end(`currentTime: ${currentTime}, version: 5, log: added nrk`)
 }
