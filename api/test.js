cfb85a29397cf92758a5ce86531d5dfe3e061ca7
Jordi Vives <jordivives94@gmail.com>
2026-02-19 16:35:46 +0100

    fix: add stiftung-warentest, NBR and the livingdocs-website downstreams

--- api/test.js
+++ api/test.js
@@ -2,5 +2,5 @@ const moment = require('moment')
 
 module.exports = (req, res) => {
   const currentTime = moment().format('MMMM Do YYYY, h:mm:ss a')
-  res.end(`currentTime: ${currentTime}, version: 5, log: added nrk`)
+  res.end(`currentTime: ${currentTime}, version: 6, log: added stiftung warentest, nbr, website, removed old sz, blz downstreams`)
 }
